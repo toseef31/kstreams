0ed8681f65d0266d744145a4eab5688c2cabe3e7
saadahmed <saad.ahmed@rockvillegroup.com>
2019-08-13 01:35:24 +0500

    imp

--- public/webRtc/server.js
+++ public/webRtc/server.js
@@ -29,7 +29,7 @@ var https = require('https');
 
 var argv = minimist(process.argv.slice(2), {
   default: {
-      as_uri: "https://www.jobcallme.com:8443/",
+      as_uri: "https://127.0.0.1:8443/",
       ws_uri: "ws://localhost:8888/kurento"
   }
 });

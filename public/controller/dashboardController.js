3b145ad287260779ac529a0980629aa624ba2eb3
saadahmed <saad.ahmed@rockvillegroup.com>
2019-09-25 06:08:07 +0500

    imp

--- public/controller/dashboardController.js
+++ public/controller/dashboardController.js
@@ -48,52 +48,7 @@ app.controller("dashController", function ($scope, $http, $window, $location, $r
         $rootScope.O2OSoc.close();
         $rootScope.O2MSoc.close();
     } 
-
-    let hostIs = location.host.split(':');
-    let webSocketIp =  $rootScope.projectData.domainUrl;  //localhost || www.jobcallme.com 
-    if(hostIs[0]=='localhost') webSocketIp='127.0.0.1';
-    let reqUrl='wss://'+webSocketIp+':8443/one2one';
-    let broadCastUrl='wss://'+webSocketIp+':8444/one2many';
-    console.log('WebSocket: ',reqUrl);
-    
-    $rootScope.O2OSoc= $websocket.$new(reqUrl); 
-    $rootScope.O2MSoc= $websocket.$new(broadCastUrl);
-    $rootScope.O2MSoc.$on('$open', function () {
-        console.log('O2M socket open'); 
-    })
-    .$on('$message', function (message) { // it listents for 'incoming event'
-        var parsedMessage = JSON.parse(message);  
-
-        switch (parsedMessage.id) {
-            case 'presenterResponse':
-                One2ManyCall.presenterResponse(parsedMessage);
-                break;
-            case 'viewerResponse':
-                One2ManyCall.viewerResponse(parsedMessage);
-                break;
-            case 'stopCommunication':
-                One2ManyCall.dispose();
-                break;
-            case 'iceCandidate':
-                $rootScope.webRtcO2MPeer.addIceCandidate(parsedMessage.candidate)
-                break;  
-            case 'presenterDataResp': 
-                    if(!$rootScope.user) break;
-                    let presenterData=[];
-                    
-                    parsedMessage.data.forEach(preData => {
-                        if(preData.preId!=$rootScope.user._id) presenterData.push(preData)
-                    }); 
-                  
-                    $rootScope.presenterArr=presenterData;
-            break;
-            default:
-                console.error('Unrecognized message', parsedMessage);
-        }
-    });
-    
-    //One2ManyCall.video = document.getElementById('broadCastVideo');  
-    //console.log(One2ManyCall.video);
+  
 
     // initial websocket connection is in loginController   
     $interval(One2ManyCall.getPresenterData, 6000);
